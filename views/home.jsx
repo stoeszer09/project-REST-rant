d6ea9c5612e95b5c2c3f96be56c360a0d2616e1e
Phil Splettstoeszer <stoeszer09@gmail.com>
2022-03-16 12:05:40 -0500

    adding bootstrap and photos

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home() {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/panini.jpg" alt="panini sandwich" />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@riaquartz?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ria Quartz</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
