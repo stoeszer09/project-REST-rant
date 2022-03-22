e928d8c98e6c86e9b8d0996949094eba513871ff
Phil Splettstoeszer <stoeszer09@gmail.com>
2022-03-22 10:57:05 -0500

    Add showEstablished method and use in Show.jsx

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,22 +5,17 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>
-                { data.place.name }
-            </h1>
             <div className="row">
-                <img src={data.place.pic} alt={data.place.name} className="col-sm-6"/>
                 <div className="col-sm-6">
-                    <h2>
-                        Description
-                    </h2>
-                    <p className="text-center">
-                        {data.place.cuisines}
-                    </p>
-
-                    <p className="text-center">
-                        Located in {data.place.city}, {data.place.state}
-                    </p>
+                    <img src={data.place.pic} alt={data.place.name}/>
+                    <h3 className="text-center">
+                            Located in {data.place.city}, {data.place.state}
+                    </h3>
+                </div>
+                <div className="col-sm-6">
+                    <h1>
+                        { data.place.name }
+                    </h1>
                     <div>
                         <h2>
                             Rating
@@ -29,26 +24,38 @@ function show (data) {
                             Not Rated
                         </p>
                     </div>
+
+                    <h2>
+                        Description
+                    </h2>
+                    <h3>
+                        {data.place.showEstablished()}
+                    </h3>
+                    <h4 className="text-center">
+                        Serving {data.place.cuisines}
+                    </h4>
+
                     <div>
-                        <h2>
-                            Comments
-                        </h2>
-                        <p>
-                            No comments yet
-                        </p>
+                       <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                        Edit
+                        </a>
+                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                            <button type="submit" className="btn btn-danger">
+                                Delete
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
             <div>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                    Edit
-                </a>
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                    <button type="submit" className="btn btn-danger">
-                        Delete
-                    </button>
-                </form>
+                <h2>
+                    Comments
+                </h2>
+                    <p>
+                        No comments yet
+                    </p>
             </div>
+            
           </main>
         </Def>
     )
