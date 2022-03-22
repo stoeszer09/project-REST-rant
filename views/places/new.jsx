f8ee1107f2e0c7231261ab1088b0df88ded1b04c
Phil Splettstoeszer <stoeszer09@gmail.com>
2022-03-22 11:22:20 -0500

    Adding some front-end validation

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -9,29 +9,68 @@ function new_form() {
                 <form method="POST" action="/places">
                     <div className="form-group">
                         <label htmlFor="name">Place Name</label>
-                        <input className="form-control" id="name" name="name" required/>
+                        <input 
+                        className="form-control" 
+                        id="name" 
+                        name="name" 
+                        required
+                        />
                     </div>
+
                     <div className="form-group">
                         <label htmlFor="pic">Picture</label>
-                        <input className="form-control" type="url" id="pic" name="pic" />
+                        <input 
+                        className="form-control" 
+                        type="url" 
+                        id="pic" 
+                        name="pic" 
+                        />
                     </div>
+
                     <div className="form-group">
                         <label htmlFor="city">City</label>
-                        <input className="form-control" id="city" name="city" />
+                        <input 
+                        className="form-control" 
+                        id="city" 
+                        name="city" 
+                        />
                     </div>
+
                     <div className="form-group">
                         <label htmlFor="state">State</label>
-                        <input className="form-control" id="state" name="state" />
+                        <input 
+                        className="form-control" 
+                        id="state" 
+                        name="state" 
+                        />
                     </div>
+
                     <div className="form-group">
                         <label for="founded">Founded Year</label>
-                        <input className="form-control" id="founded" name="founded" />
+                        <input 
+                        type="number"
+                        className="form-control" 
+                        id="founded" 
+                        name="founded"
+                        value={new Date().getFullYear()} 
+                        />
                     </div>
+
                     <div className="form-group">
                         <label htmlFor="cuisines">Cuisines</label>
-                        <input className="form-control" id="cuisines" name="cuisines" required/>
+                        <input 
+                        className="form-control" 
+                        id="cuisines" 
+                        name="cuisines" 
+                        required
+                        />
                     </div>
-                    <input className="btn btn-primary" type="submit" value="Add Place" />
+
+                    <input 
+                    className="btn btn-primary" 
+                    type="submit" 
+                    value="Add Place" 
+                    />
                 </form>
             </main>
         </Def>
